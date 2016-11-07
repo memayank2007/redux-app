0b129149ebedb60d6faabb1c14e47187b3a94218
memayank2007 <memayank2007@gmail.com>
2016-11-07 20:10:52 +0530

    layout completed

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const PhotoGrid = React.createClass({
+	render(){
+
+		return(
+			<div>
+				I'm Photo grid
+			</div>
+			)
+	}
+
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Single = React.createClass({
+	render(){
+
+		return(
+			<div>
+				I'm a sigle
+			</div>
+			)
+	}
+
+});
+
+export default Single;
