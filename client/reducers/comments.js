4aced69028457f985118194239cb413e6031b867
memayank2007 <memayank2007@gmail.com>
2016-11-08 22:21:27 +0530

    remove comments from list

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,9 +1,33 @@
-function comments(state= [] , action){
-	console.log('this post will change')
-	console.log(state , action);
-	return state;
+function postComments(state = [], action) {
+  switch(action.type){
+    case 'ADD_COMMENT':
+       return [...state,{
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+       return [
+         ...state.slice(0,action.i),
+ 
+         ...state.slice(action.i + 1)
+      ]
+    default:
+      return state;
+  }
+  return state;
+}
+
+function comments(state = [], action) {
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+  return state;
 }
 
 export default comments;
-
 
